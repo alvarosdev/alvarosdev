24984fcfd36db382bbbd2fd51ddad6805b011f55
Alvaro Godoy <hola@asgg.cl>
2024-02-10 02:30:06 -0300

    First commit

---
+++ src/components/index.js
@@ -0,0 +1,13 @@
+import Card from './Card.astro'
+import Education from './Education.astro'
+import Job from './Job.astro'
+import Skill from './Skill.astro'
+import Volunteer from './Volunteer.astro'
+
+export { 
+  Card,
+  Education,
+  Job,
+  Skill,
+  Volunteer
+}
