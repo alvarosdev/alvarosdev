d34357493f760e4ccde024a563b6f32038b90fbc
Alvaro Godoy <hola@asgg.cl>
2024-02-10 17:44:04 -0300

    Add React integration and update tsconfig.json and astro.config.mjs

--- src/components/index.js
+++ src/components/index.js
@@ -3,11 +3,13 @@ import Education from './Education.astro'
 import Job from './Job.astro'
 import Skill from './Skill.astro'
 import Volunteer from './Volunteer.astro'
+import Button from './Button.astro'
 
 export { 
   Card,
   Education,
   Job,
   Skill,
-  Volunteer
+  Volunteer,
+  Button
 }
